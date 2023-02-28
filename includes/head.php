29a5ab1dbbc399441f0be0374f2083f71bc207a5
Surha <surha_dag@mail.ru>
2023-02-28 11:14:32 +0300

    добавил запрет индексации

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
     <meta name="keywords" content="Sona, unica, creative, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="noindex" />
     <title>Sona | Template</title>
 
     <!-- Google Font -->
