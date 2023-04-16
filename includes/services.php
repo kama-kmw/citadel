03fe2db5c9fac6500d3e675c536bdaeae9c35cc5
Surha <surha_dag@mail.ru>
2023-04-16 17:04:49 +0300

    add service to front page

--- includes/services.php
+++ includes/services.php
@@ -1,56 +1,27 @@
 <section class="services-section">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="section-title">
-              <span>Сервис</span>
-              <h2>Наши услуги</h2>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 col-sm-6">
-            <div class="service-item">
-              <i class="flaticon-036-parking"></i>
-              <h4>Парковка</h4>
-              <p>Удобная парковка для клиентов</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <div class="service-item">
-              <i class="flaticon-033-dinner"></i>
-              <h4>Столовая</h4>
-              <p>Вкусные национальные блюда</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <div class="service-item">
-              <i class="flaticon-026-bed"></i>
-              <h4>Обслуживание в номерах</h4>
-              <p>Всегда чистота и порядок в номерах.</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <div class="service-item">
-              <i class="flaticon-024-towel"></i>
-              <h4>Смена белья</h4>
-              <p>Всегда свежее постельное белье.</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <div class="service-item">
-              <i class="flaticon-044-clock-1"></i>
-              <h4>Круглосуточное обслуживание</h4>
-              <p>Наши клиенты всегда в приоритете</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-sm-6">
-            <div class="service-item">
-              <i class="flaticon-012-cocktail"></i>
-              <h4>Подача в номер</h4>
-              <p>Подача напитков и еды в номер</p>
-            </div>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="section-title">
+          <span>Сервис</span>
+          <h2>Наши услуги</h2>
         </div>
       </div>
-    </section>
+    </div>
+    <div class="row">
+      <?php
+      $catalog_list = mysqli_query($connection, 'SELECT * FROM `service` ORDER BY `id`');
+      while ($catalog_item = mysqli_fetch_assoc($catalog_list)) {
+        echo '
+            <div class="col-lg-4 col-sm-6">
+              <div class="service-item">
+                <i class="' . $catalog_item["icon"] . '"></i>
+                <h4>' . $catalog_item["title"] . '</h4>
+                <p>' . $catalog_item["description"] . '</p>
+              </div>
+            </div>';
+      }
+      ?>
+    </div>
+  </div>
+</section>
